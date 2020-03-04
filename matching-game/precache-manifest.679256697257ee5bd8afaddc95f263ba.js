a2971b13d9101c71ed9eba4a13d7fea0610fb5e7
Bo <wipada@vertice-international.com>
2020-03-04 15:02:07 +0700

    add matching game

---
+++ matching-game/precache-manifest.679256697257ee5bd8afaddc95f263ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4c946c04e3404f025b77edfdd8394e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a0153be9b3bb1965d62",
+    "url": "/static/css/main.96db1141.chunk.css"
+  },
+  {
+    "revision": "50d23952d7fd1b97712b",
+    "url": "/static/js/2.323a7da6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.323a7da6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0153be9b3bb1965d62",
+    "url": "/static/js/main.79cd846a.chunk.js"
+  },
+  {
+    "revision": "34a3d4d79698c1573c1b",
+    "url": "/static/js/runtime-main.6a6d2fe2.js"
+  }
+]);
